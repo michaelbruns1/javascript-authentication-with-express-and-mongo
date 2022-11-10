b7c02667734063716856f21280bea6847215d78b
michaelbruns1 <mb@michaelbruns.one>
2022-11-10 12:10:14 -0800

    Update 2 files

--- app.js
+++ app.js
@@ -15,36 +15,36 @@ db.once('open', function() {
 
 
 // use sessions for tracking logins
-var session = require('express-session');
-app.use(session( {
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: false
+var session = require('express-session'); // npm install express-session
+app.use(session( { 
+  secret: process.env.SESSION_SECRET, // secret key
+  resave: true,  // save session even if not modified
+  saveUninitialized: false // don't save unmodified session
 }));
 
 // make user ID available in templates
-app.use(function (req, res, next) {
-  res.locals.currentUser = req.session.userId;
+app.use(function (req, res, next) { 
+  res.locals.currentUser = req.session.userId; // set local variable
   next();
 });
 
 // parse incoming requests
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // parse json
+app.use(express.urlencoded({ extended: true })); // parse form data
 
 // serve static files from /public
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); // serve static files
 
 // view engine setup
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
+app.set('view engine', 'pug'); // set up pug
+app.set('views', __dirname + '/views'); // set up views folder
 
 // include routes
-var routes = require('./routes/index.js');
-app.use('/', routes);
+var routes = require('./routes/index.js'); // include routes
+app.use('/', routes); // use routes
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next) { // 404 error handler
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
@@ -52,8 +52,8 @@ app.use(function(req, res, next) {
 
 // error handler
 // define as the last app.use callback
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+app.use(function(err, req, res, next) { // error handler
+  res.status(err.status || 500); 
   res.render('error', {
     message: err.message,
     error: {}
@@ -61,6 +61,6 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 3000
-app.listen(3000, function () {
+app.listen(3000, function () { // listen on port 3000
   console.log('Express app listening on port 3000');
 });
--- middleware/index.js
+++ middleware/index.js
@@ -5,13 +5,13 @@ function loggedOut(req, res, next) {
   return next();
 }
 function requiresLogin(req, res, next) {
-  if (req.session && req.session.userId) {
-    return next();
-  } else {
-    var err = new Error('You must be logged in to view this page.');
-    err.status = 401;
-    return next(err);
+  if (req.session && req.session.userId) { // if user is logged in
+    return next(); // allow the next route to run
+  } else { 
+    var err = new Error('You must be logged in to view this page.'); // create error
+    err.status = 401; // set error status
+    return next(err); // pass error to next middleware
   }
 }
-module.exports.loggedOut = loggedOut;
-module.exports.requiresLogin = requiresLogin;
+module.exports.loggedOut = loggedOut; // export loggedOut function
+module.exports.requiresLogin = requiresLogin; // export requiresLogin function
